986def03072b764e8a5ad21ff5a062cfccbb001e
Chris Whatley <cwhatley@western-bp.co.uk>
2020-06-10 10:52:12 +0100

    Dev config & initial build

--- config.php
+++ config.php
@@ -2,7 +2,8 @@
 
 return [
     'production' => false,
-    'baseUrl' => '',
-    'siteTitle' => 'Jigsaw + Tailwind CSS Starter Kit',
+    'baseUrl' => 'http://localhost:3000',
+    'siteTitle' => 'Western Business Media',
+    'siteDescription' => 'Western Business Media publish leading business-to-business titles across a wide variety of UK sectors as well as being the organisers of the prestigious Safety & Health Excellent Awards, Tomorrows Warehouse & Air User Live events.',
     'collections' => [],
 ];
